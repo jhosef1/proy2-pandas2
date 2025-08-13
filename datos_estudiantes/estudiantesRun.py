bb1e6e83e8c4adc8283e3acd5ae14a788d44e5b3
josafat <josafatmamani12@gmail.com>
2025-08-13 12:48:21 -0400

    codigo actualizado estudiante

--- datos_estudiantes/estudiantesRun.py
+++ datos_estudiantes/estudiantesRun.py
@@ -4,5 +4,12 @@ df = pd.read_csv("estudiantes.csv")
 
 print("Tabla Completa:")
 print(df)
-print("\n📌 Alumnos con calificación mayor a 8:")
+print("\n📌 Alumnos con calificación mayor a 90:")
 print(df[df['Notas'] > 90])
+print("\n📌 Alumnos con calificación menor a 90:")
+print(df[df['Notas'] < 90])
+
+nota_maxima = df['Notas'].max()
+nota_min = df['Notas'].min()
+print(f"\n Nota Maxima: {nota_maxima}")
+print(f"\n Nota Maxima: {nota_min}")
